85ea196817a47856406cb6678ef4f8938a473ced
Jasmine Xie (MSFT) <v-lilx@microsoft.com>
2021-01-29 05:59:15 +0000

    dsdsas

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Test();
+		}
+		static void Test()
+		{
+			try
+			{
+				Exception();
+			}
+			catch (Exception ex)
+			{
+
+				throw new NullReferenceException("This is an unhandled exception", ex);
+			}
+		}
+
+		static void Exception()
+		{
+			int a = 0;
+			int b = 1 / a;
+		}
+	}
+}
